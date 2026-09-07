236867f6e1e2a5de74c5ec814afe920f4921ac9e
QDnh <qdonahe1@buffs.wtamu.edu>
2026-09-06 23:03:53 -0500

    Display the list on the home page

--- pages/views.py
+++ pages/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
 
+mylist = [
+    {"title": "Buy groceries", "done": True},
+    {"title": "Get gas", "done": True},
+    {"title": "Mow lawn", "done": False},
+    {"title": "Work on project", "done": True},
+    {"title": "Take quiz", "done": False},
+]
+
+
 def home(request):
-    return render(request, "home.html")
+    return render(request, "home.html", {"mylist": mylist})
 
 def about(request):
     return render(request, "about.html")
